c8a2f8b9b894c3b4ee1a4bf6ea8416570f828ec0
Conor Casey <16ccasey@student.kenmarecs.com>
2018-12-23 14:23:30 +0000

    Update Program_3.py

--- Program_3.py
+++ Program_3.py
@@ -29,8 +29,9 @@ v1.1.0: Let's Get Py Crazy! update.
 v1.1.1: Small Changes and Fixes
 v1.2.0: Major Changes to Data Calculations
 v1.5.0: Fin 2018 update
+v1.5.1: Emergency Bug Fix Update
 
-Current Edition: v1.5.0""")
+Current Edition: v1.5.1""")
 
 def main():
 	#Imports Population Density Data from the Central Statistics Office
